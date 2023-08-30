ca1516303e0fc61bbf75421ae42c1f6b2d6e26b3
samjwu <sjwu@ualberta.ca>
2023-08-29 18:58:16 -0600

    2629

---
+++ 2629/Solution.ts
@@ -0,0 +1,18 @@
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+	return function(x) {
+        let ans: number = x;
+        
+        for (let i = functions.length-1; i >= 0; i--) {
+            ans = functions[i](ans);
+        }
+        
+        return ans;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
